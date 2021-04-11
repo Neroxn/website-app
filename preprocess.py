e4cb9e60d808f504815a100dd50dd23333dde821
Okan Mercan <okanmercan16@gmail.com>
2021-04-11 22:18:01 +0300

    Preprocess functions are written
    Should be checked

--- preprocess.py
+++ preprocess.py
@@ -15,3 +15,15 @@ def stringEncoder(df, columns):
 		arrEncoder.append((column, encoder))
 	
 	return df, arrEncoder
+
+def dropNanAndDuplicates(df, notNanPercantage = 1)
+    threshval = int(len(df) * notNanPercantage)
+    df = df.dropna(thresh= threshval, axis=1)
+    df = df.drop_duplicates()
+    return df
+
+def scale(df)
+    scaler = MinMaxScale(feature_range=(0,1))
+    scaled = scaler.fit_transform(df)
+    df = pd.DataFrame(scaled, columns= df.columns, index= df.index)
+    return df, scaler
