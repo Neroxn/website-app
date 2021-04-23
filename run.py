314796b56caf4c139713eb1b344deef8ff2bf7f1
Bora Kargı <44094497+Neroxn@users.noreply.github.com>
2021-04-23 16:08:07 +0300

    Add files via upload

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 import os
 import numpy as np  
 import pandas as pd
+from bokeh.models import ColumnDataSource, Div, Select, Slider, TextInput
+from bokeh.io import curdoc
+from bokeh.resources import INLINE
+from bokeh.embed import components
+from bokeh.plotting import figure, output_file, show
 from flask import Flask, request, redirect, url_for,render_template
 from werkzeug.utils import secure_filename
-from helper.utils import *
-
-UPLOAD_FOLDER = 'C:\\Users\\kargi\\Flask Practi e\\basic-upload\\datasets'
+from utils import *
+from modelTrain import *
+from preprocess import *
+from sklearn.model_selection import train_test_split
+UPLOAD_FOLDER = 'C:\\Users\\kargi\\Flask Practi e\\website-app-main-2\\datasets'
 ALLOWED_EXTENSIONS = set(['txt', 'csv'])
 
 #Global variables that can bee accesed
@@ -95,9 +102,6 @@ def select_y():
         cols = []
     return render_template("select_variables.html",types = dtypes, columns = cols)
 
-@app.route("/visualize", methods = ["GET","POST"])
-def visualize():
-    return render_template("visualize.html")
 
 @app.route("/selectAlgo", methods = ["GET","POST"])
 def selectAlgo():
@@ -118,23 +122,283 @@ def selectAlgo():
 @app.route("/SVM", methods = ["GET","POST"])
 def SVM():
     if request.method == 'POST':
-        return "To be continued"
+        #Check whether kernel is valid
+        if not request.form['kernel'] in ['linear', 'rbf', 'poly','sigmoid']:
+            flash('Invalid kernel, kernel must be one of them: linear, rbf, poly, sigmoid')
+            return render_template("algorithms/SVM.html")
+        else:
+            kernel = request.form['kernel']
+            
+        #Check whether C is valid
+        try:
+            if float(request.form['C']) <= 0 :
+                flash('C must be positive real number')
+                return render_template("algorithms/SVM.html")
+            else:
+                C = request.form['C']
+        except:
+            flash('C must be a float number')
+            return render_template("algorithms/SVM.html")
+            
+        #Check whether gamma is valid
+        if(kernel == 'linear'):
+            gamma = 'scale'
+        try:
+            if request.form['gamma'] == 'scale':
+                gamma = 'scale'
+            elif request.form['gamma'] == 'auto':
+                gamma = 'auto'
+            elif float(request.form['gamma']) == 0:
+                flash('Gamma must be different from 0')
+                return render_template("algorithms/SVM.html")
+            else:
+                gamma = float(request.form['gamma'])
+        except:
+            flash('Gamma must be auto, scale, or a float number')
+            return render_template("algorithms/VM.html")
         
-    return render_template("SVM.html")
+        #Check whether degree is valid
+        if(kernel != 'poly' and request.form['degree'] != ''):
+            flash('Poly must be defined if kernel is polynomial')
+            return render_template("algorithms/SVM.html")
+        if(kernel == 'poly'):
+            try:
+                if int(request.form['degree']) <= 0:
+                    flash('Degree must be positive integer')
+                    return render_template("algorithms/SVM.html")
+                else:
+                    degree = int(request.form['degree'])
+                
+            except:
+                flash('Degree must be a positive integer')
+                return render_template("algorithms/SVM.html")
+        else:
+            degree = 3
+            
+        #Parameters are valid, train time
+        df2 = df[selectedX+selectedY]
+        df2 = dropNanAndDuplicates(df2, 0.75)
+        df2, encoderArr = stringEncoder(df2, df2.loc[:, df2.dtypes == object].columns)
+        df2, scaler, scalerY = scale(df2, selectedY)
+        trainX, testX, trainY, testY = train_test_split(df2[selectedX], df2[selectedY], 
+                                                        test_size= 0.15, shuffle= True)
+        
+        model = applySVM(trainX, trainY, kernel= kernel, c= float(C), gamma= gamma, degree= float(degree))
+        
+        #Train is done, predict time
+        result = model.predict(testX).reshape((len(testX),-1))
+        print(result.shape)
+        result = scalerY.inverse_transform(result)
+        if encoderArr:
+            result = stringDecoder(result, encoderArr, selectedY)
+        
+        return redirect(url_for('results', actual= testY, prediction= result))
+        
+    return render_template("algorithms/SVM.html")
 
 @app.route("/RandomForest", methods = ["GET","POST"])
 def RandomForest():
     if request.method == 'POST':
-        return "To be continued"
+    
+        #Check whether number of estimator is valid
+        try:
+            if int(request.form['numberEstimator']) <= 0 :
+                flash('Number of estimator must be a positive integer')
+                return render_template("algorithms/RandomForest.html")
+            else:
+                numberEstimator = int(request.form['numberEstimator'])
+        except:
+            flash('Number of estimator must be an integer')
+            return render_template("algorithms/RandomForest.html")
+            
+        #Check whether maxDepth is valid
+        try:
+            if request.form['maxDepth'] == 'None':
+                maxDepth = 'None'
+            elif int(request.form['maxDepth']) <= 0 :
+                flash('Max depth must be a positive integer')
+                return render_template("algorithms/RandomForest.html")
+            else:
+                maxDepth = int(request.form['maxDepth'])
+        except:
+            flash('Max depth must be an integer or None')
+            return render_template("algorithms/RandomForest.html")
+            
+        #Check whether minimum samples leaf is valid
+        try:
+            if int(request.form['minSamplesLeaf']) <= 0 :
+                flash('Minimum samples leaf must be a positive integer or float')
+                return render_template("algorithms/RandomForest.html")
+            else:
+                minSamplesLeaf = int(request.form['minSamplesLeaf'])
+        except:
+            try:
+                if float(request.form['minSamplesLeaf']) <= 0 :
+                    flash('Minimum samples leaf must be a positive integer or float')
+                    return render_template("algorithms/RandomForest.html")
+                else:
+                    minSamplesLeaf = float(request.form['minSamplesLeaf'])
+            except:
+                flash('Minimum samples leaf must be an integer or float')
+                return render_template("algorithms/RandomForest.html")
+                
+        #Parameters are valid, train time
+        df2 = df[selectedX+selectedY]
+        df2 = dropNanAndDuplicates(df2, 0.75)
+        df2, encoderArr = stringEncoder(df2, df2.loc[:, df2.dtypes == object].columns)
+        trainX, testX, trainY, testY = train_test_split(df2[selectedX], df2[selectedY], 
+                                                        test_size= 0.15, shuffle= True)
+        model = applyRandomForest(trainX, trainY, numberEstimator= numberEstimator, maxDepth = maxDepth, minSamplesLeaf=minSamplesLeaf)
         
-    return render_template("RandomForest.html")
+        #Train is done, predict time
+        result = model.predict(testX).reshape((len(testX),-1))
+        if encoderArr:
+            result = stringDecoder(result, encoderArr, selectedY)
+        
+        return redirect(url_for('results', actual= testY, prediction= result))
+    return render_template("algorithms/RandomForest.html")
 
 @app.route("/Adaboost", methods = ["GET","POST"])
 def Adaboost():
     if request.method == 'POST':
-        return "To be continued"
-        
-    return render_template("Adaboost.html")
     
+        #Check whether number of estimator is valid
+        try:
+            if int(request.form['numberEstimator']) <= 0 :
+                flash('Number of estimator must be a positive integer')
+                return render_template("algorithms/Adaboost.html")
+            else:
+                numberEstimator = int(request.form['numberEstimator'])
+        except:
+            flash('Number of estimator must be an integer')
+            return render_template("algorithms/Adaboost.html")
+            
+        #Check whether learning rate is valid
+        try:
+            if float(request.form['learningRate']) <= 0 :
+                flash('Learning rate must be a positive float')
+                return render_template("algorithms/RandomForest.html")
+            else:
+                learningRate = float(request.form['learningRate'])
+        except:
+            flash('Number of estimator must be an float')
+            return render_template("algorithms/RandomForest.html")
+            
+        #Check whether loss is valid
+        if not request.form['loss'] in ['linear', 'square', 'exponential']:
+            flash('Invalid loss, loss must be one of them: linear, square, exponential')
+            return render_template("algorithms/RandomForest.html")
+        else:
+            loss = request.form['loss']
+            
+        #Parameters are valid, train time
+        df2 = df[selectedX+selectedY]
+        df2 = dropNanAndDuplicates(df2, 0.75)
+        df2, encoderArr = stringEncoder(df2, df2.loc[:, df2.dtypes == object].columns)
+        trainX, testX, trainY, testY = train_test_split(df2[selectedX], df2[selectedY], 
+                                                        test_size= 0.15, shuffle= True)
+        model = applyAdaBoost(trainX, trainY, numberEstimator= numberEstimator, learningRate = learningRate, loss=loss)
+        
+        #Train is done, predict time
+        result = model.predict(testX).reshape((len(testX),-1))
+        if encoderArr:
+            result = stringDecoder(result, encoderArr, selectedY)
+        
+        return redirect(url_for('results', actual= testY, prediction= result))
+            
+    return render_template("algorithms/Adaboost.html")
+
+@app.route("/results", methods = ["GET","POST"])
+def results():
+    if request.method == "POST":
+        return "To be continued"
+
+    return "To be continued"
+        
+    prediction = request.args['prediction']
+    actual = request.args['actual']
+    return "To be continued"
+        
+    return render_template("algorithms/Adaboost.html")
+    
+@app.route('/visualize',methods = ["GET","POST"])
+def visualize():
+    print(request.method)
+    if request.method == 'POST':
+
+        if request.form['selector'] == "scatter":
+            return redirect(url_for('scatter_graph'))
+
+        elif request.form['selector'] == "corr":
+            return redirect(url_for('correlation_graph'))
+
+        elif request.form['selector'] == "pie":
+            return redirect(url_for('pie_graph'))
+
+        elif request.form['selector'] == "dist":
+            return redirect(url_for('dist_graph'))
+
+    return render_template('graphs/visualize.html', graphSelected = False)
+    
+@app.route('/scatter_graph', methods = ["GET","POST"])
+def scatter_graph():
+    global df
+    if request.method == "POST":
+        if 'parameters' in request.form:
+            selected_features = request.form.getlist('parameters')
+            
+        else:
+            return render_template('graphs/scatter_plot.html',columns = df.columns,error = "Please select parameters to process.")
+
+        if len(selected_features) >= 10:
+            return render_template('graphs/scatter_plot.html',columns = df.columns,error = "You have selected more than 10 features!. Please select again.")
+
+        elif len(selected_features) <= 2:
+            return render_template('graphs/scatter_plot.html',columns = df.columns,error = "You have selected less than 2 features!. Please select again.")
+        
+        else:
+            return scatter_matrix(df,selected_features)
+    return render_template('graphs/scatter_plot.html',columns = df.columns)
+
+
+@app.route('/correlation_graph', methods = ["GET","POST"])
+def correlation_graph():
+    global df
+    if request.method == "POST":
+        if 'parameters' in request.form:
+            selected_features = request.form.getlist('parameters')
+            return correlation_plot(df.select_dtypes(exclude = ['object']),selected_features)
+            
+        else:
+            return render_template('graphs/correlation_plot.html',columns = df.select_dtypes(exclude = ['object']).columns,error = "Please select parameters to process.")
+
+    return render_template('graphs/correlation_plot.html',columns = df.select_dtypes(exclude = ['object']).columns)
+
+
+@app.route('/pie_graph', methods = ["GET","POST"])
+def pie_graph():
+    global df
+    if request.method == "POST":
+        print(request.form)
+        if "selected_parameter" in request.form:
+            return pie_plot(df.select_dtypes(include = ["object"]),request.form['selected_parameter'])
+        else:
+            return render_template('graphs/pie_plot.html',columns =  df.select_dtypes(include = ["object"]).columns,error = "Please select parameters to process.")
+
+    return render_template('graphs/pie_plot.html',columns = df.select_dtypes(include = ["object"]).columns)
+
+@app.route('/dist_graph', methods = ["GET","POST"])
+def dist_graph():
+    global df
+    if request.method == "POST":
+        print(request.form)
+        if 'selected_parameter' in request.form:
+            numberBin = 20 if 'numberBin' not in request.form else int(request.form['numberBin'])
+            return dist_plot(df.select_dtypes(exclude = ['object']),request.form['selected_parameter'],numberBin)
+        else:
+            return render_template('graphs/dist_plot.html',columns =df.select_dtypes(exclude = ['object']).columns, error = "Please choose the parameter for histogram!")
+    return render_template('graphs/dist_plot.html',columns = df.select_dtypes(exclude = ['object']).columns)
+
+        
 if __name__ == "__main__":
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -4,6 +4,20 @@ import time
 from run import ALLOWED_EXTENSIONS
 from flask_wtf import FlaskForm
 from collections import OrderedDict
+from flask import Flask, request, redirect, url_for,render_template
+from bokeh.models import ColumnDataSource, Div, Select, Slider, TextInput
+from bokeh.io import curdoc
+from bokeh.resources import INLINE
+from bokeh.embed import components
+from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import gridplot
+import bisect
+from math import pi
+from numpy import arange
+from itertools import chain
+from collections import OrderedDict
+from bokeh.palettes import RdBu as colors
+from bokeh.models import ColorBar, LinearColorMapper
 
 def load_dataset(path,delimitter,qualifier,assumption = False):
     """
@@ -90,7 +104,7 @@ def assign_datatypes(data,dataTypes):
     """
     for i in range(len(dataTypes)):
         data.iloc[:,i] = data.iloc[:,i].astype(dataTypes[i])
-
+    return data
 
 def choose_attributes(df,attributes):
     """
@@ -124,3 +138,222 @@ def groupColumns(df):
                 arr.append(columnArr[k][i])
         returnArr.append(arr)
     return dtypeArr, returnArr
+
+
+def scatter_plot(df,x_name,y_name):
+#create the graph
+    fig = figure(plot_height=600, plot_width=720)
+    source = ColumnDataSource()
+    fig.circle(x="x", y="y", source=source, size=5,line_color=None)
+
+    #choose x and y label for plotting
+    fig.xaxis.axis_label = x_name
+    fig.yaxis.axis_label = y_name
+    source.data = dict(
+        x = df[x_name],
+        y = df[y_name]
+    )
+    script, div = components(fig)
+    #return template
+    return render_template(
+        'scatter_plot.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=INLINE.render_js(),
+        css_resources=INLINE.render_css(),
+        x_name = x_name,
+        y_name = y_name,
+        graphSelected = True,
+        columns = df.columns
+    ).encode(encoding='UTF-8')
+
+def scatter_matrix(dataset,features):
+    dataset_selected = dataset[features]
+    dataset_source = ColumnDataSource(data=dataset)
+    scatter_plots = []
+    y_max = len(dataset_selected.columns)-1
+    for i, y_col in enumerate(dataset_selected.columns):
+        for j, x_col in enumerate(dataset_selected.columns):
+            p = figure(plot_width=100, plot_height=100, x_axis_label=x_col, y_axis_label=y_col)
+            p.circle(source=dataset_source,x=x_col, y=y_col, fill_alpha=0.3, line_alpha=0.3, size=3)
+            if j > 0:
+                p.yaxis.axis_label = ""
+                p.yaxis.visible = False
+                p.y_range = linked_y_range
+            else:
+                linked_y_range = p.y_range
+                p.plot_width=240
+            if i < y_max:
+                p.xaxis.axis_label = ""
+                p.xaxis.visible = False
+            else:
+                p.plot_height=160
+            if i > 0:
+                p.x_range = scatter_plots[j].x_range
+
+            scatter_plots.append(p)
+
+    grid = gridplot(scatter_plots, ncols = len(dataset_selected.columns))
+
+    script, div = components(grid)
+    return render_template(
+    'graphs/scatter_plot.html',
+    plot_script=script,
+    plot_div=div,
+    js_resources=INLINE.render_js(),
+    css_resources=INLINE.render_css(),
+    x_name = "t1",
+    y_name = "t2",
+    graphSelected = True,
+    columns = dataset.columns
+    ).encode(encoding='UTF-8')
+
+
+def correlation_plot(df,selected_parameters):
+
+    import pandas as pd
+    from bokeh.io import output_file, show
+    from bokeh.models import BasicTicker, ColorBar, LinearColorMapper, ColumnDataSource, PrintfTickFormatter
+    from bokeh.plotting import figure
+    from bokeh.transform import transform
+    from bokeh.palettes import Viridis256
+
+    # Read your data in pandas dataframe
+    data = df[selected_parameters]
+    #Now we will create correlation matrix using pandas
+    data_corr = data.corr()
+
+    data_corr.index.name = 'AllColumns1'
+    data_corr.columns.name = 'AllColumns2'
+
+    # Prepare data.frame in the right format
+    data_corr = data_corr.stack().rename("value").reset_index()
+    print(selected_parameters)
+
+    # I am using 'Viridis256' to map colors with value, change it with 'colors' if you need some specific colors
+    mapper = LinearColorMapper(
+        palette=Viridis256, low=data_corr.value.min(), high=data_corr.value.max())
+
+    # Define a figure and tools
+    TOOLS = "box_select,lasso_select,pan,wheel_zoom,box_zoom,reset,help"
+    p = figure(
+        tools=TOOLS,
+        plot_width=1500,
+        plot_height=1250,
+        title="Correlation plot",
+        x_range=list(data_corr.AllColumns1.drop_duplicates()),
+        y_range=list(data_corr.AllColumns2.drop_duplicates()),
+        toolbar_location="right",
+        x_axis_location="below")
+
+    # Create rectangle for heatmap
+    p.rect(
+        x="AllColumns1",
+        y="AllColumns2",
+        width=1,
+        height=1,
+        source=ColumnDataSource(data_corr),
+        line_color=None,
+        fill_color=transform('value', mapper))
+        
+    p.xaxis.major_label_orientation = "vertical"
+    # Add legend
+    color_bar = ColorBar(
+        color_mapper=mapper,
+        location=(0, 0),
+        ticker=BasicTicker(desired_num_ticks=10))
+
+    p.add_layout(color_bar, 'right')
+
+    script, div = components(p)
+    return render_template(
+    'graphs/correlation_plot.html',
+    plot_script=script,
+    plot_div=div,
+    js_resources=INLINE.render_js(),
+    css_resources=INLINE.render_css(),
+    graphSelected = True,
+    columns = df.columns
+    ).encode(encoding='UTF-8')
+
+def pie_plot(data,selected_parameter, sort_by_values = False):
+    from math import pi 
+    from bokeh.transform import cumsum 
+    from bokeh.palettes import inferno
+    df_pie_agg = pd.DataFrame()
+    
+    objects_data = data[selected_parameter].unique()
+    val_count = []
+
+    for vals in objects_data:
+        val_count += [data[data[selected_parameter] == vals].shape[0]]
+    print(objects_data,val_count)
+    df_pie_agg = pd.DataFrame()
+    df_pie_agg["Parameter"] = objects_data
+    df_pie_agg["Count"] = val_count
+    print(val_count)
+    # Add angles based on Win Count so each wedge is the right size
+    df_pie_agg['Angle'] = df_pie_agg['Count']/df_pie_agg['Count'].sum() * 2*pi
+    df_pie_agg['color'] = inferno(len(val_count))
+    
+    if sort_by_values:
+        df_pie_agg = df_pie_agg.sort_values(by = 'Count', ascending = False)
+
+    # Draw a chart
+    p = figure(title='Pie Chart', x_range=(-0.5, 1.0),
+            plot_width=800, plot_height=600, 
+            toolbar_location=None, tooltips="@Parameter: @{Count}") 
+
+    p.wedge(x=0.1, y=1, radius=0.4,
+            start_angle=cumsum('Angle', include_zero=True), 
+            end_angle=cumsum('Angle'),
+            line_color="white", 
+            legend='Parameter', 
+            fill_color = 'color',
+            source=df_pie_agg)
+
+    # These options can be used to make the chart cleaner
+    #p.axis.axis_label=None
+    #p.axis.visible=False
+    #p.grid.grid_line_color = None
+    #p.outline_line_color = None
+
+    script, div = components(p)
+    return render_template(
+    'graphs/pie_plot.html',
+    plot_script=script,
+    plot_div=div,
+    js_resources=INLINE.render_js(),
+    css_resources=INLINE.render_css(),
+    graphSelected = True,
+    selected = selected_parameter,
+    columns = data.columns
+    ).encode(encoding='UTF-8')
+    return "sex"
+
+def dist_plot(df,parameter,bins = 20):
+    
+    # Use numpy to create histogram bins
+    hist, edges = np.histogram(df[parameter], bins=bins)
+
+    # Draw a chart
+    p = figure(title='Histogram', plot_width=800, plot_height=600,
+            x_axis_label=parameter, y_axis_label='Count')
+
+    p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:], line_color='white', fill_color='black')
+
+
+    script, div = components(p)
+    return render_template(
+    'graphs/dist_plot.html',
+    plot_script=script,
+    plot_div=div,
+    js_resources=INLINE.render_js(),
+    css_resources=INLINE.render_css(),
+    graphSelected = True,
+    columns = df.columns,
+    selected = parameter
+    ).encode(encoding='UTF-8')
+
+def nan_plot(df):
+    return "Show Nan"
